57f7fe8d2dea50746b1e0735c951c49b7f5f6e9f
WilliamHWick <wiwic6114@hvl.no>
2026-01-25 22:38:49 +0100

    test

--- oppgave 1/src/no/hvl/data102/impl/Sjanger.java
+++ oppgave 1/src/no/hvl/data102/impl/Sjanger.java
@@ -1,3 +1,15 @@
-public enum Sjanger {
+package no.hvl.data102;
 
+public enum Sjanger {
+    ACTION, DRAMA, HISTORY, SCIFI;
+
+    public static Sjanger finnSjanger(String navn) {
+        for (Sjanger s : Sjanger.values()) {
+            if (s.toString().equals(navn.toUpperCase())) {
+                return s;
+            }
+        }
+        return null;
+    }
 }
+
